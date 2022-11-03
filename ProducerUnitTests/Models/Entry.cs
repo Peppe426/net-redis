4e60b6f906d2f5d9fefbcf7406b39a91d4165612
Bar <barbarossa426@gmail.com>
2022-11-03 07:39:02 +0100

    test(ProducerTest) add test for producer

---
+++ ProducerUnitTests/Models/Entry.cs
@@ -0,0 +1,9 @@
+﻿namespace ProducerUnitTests.Models;
+public struct Entry
+{
+    public Entry(string message)
+    {
+        Message = message;
+    }
+    public string Message { get; private set; }
+}
