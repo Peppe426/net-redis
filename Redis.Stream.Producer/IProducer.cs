fcda8418f9f00595d1b9b7ee298de28e566702db
Bar <barbarossa426@gmail.com>
2022-11-03 07:37:02 +0100

    feature(IProducer) added contract

---
+++ Redis.Stream.Producer/IProducer.cs
@@ -0,0 +1,6 @@
+﻿namespace Redis.Stream.Producer;
+
+public interface IProducer<T> where T : struct
+{
+    Task<(bool isSuccess, string response)> EmitEntryToStream(T entry, CommandFlags commandFlags = CommandFlags.None);
+}
