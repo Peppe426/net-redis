d980939af4649ff42ff5ca65ccb893fc824a0b22
Peter <peter.liljeholm@axakon.se>
2023-02-01 19:22:50 +0100

    feat(Consumer) added .net consumer

---
+++ Redis.Stream.Consumer/ConsumerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using SharedNET;
+
+namespace Redis.Stream.Consumer;
+
+public record ConsumerConfiguration : RedisConfiguration
+{
+    public ConsumerConfiguration(string connectionString, string streamName, string streamField, string consumerGroup) : base(connectionString, streamName, streamField)
+    {
+        ConsumerGroup = consumerGroup;
+    }
+
+    public string ConsumerGroup { get; private set; }
+}
