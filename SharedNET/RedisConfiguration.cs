cf36002263be9fc4bbf97affc8a7ae56f438b178
Peter <peter.liljeholm@axakon.se>
2023-02-01 19:21:51 +0100

    feat(shared) added shared project

---
+++ SharedNET/RedisConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace SharedNET;
+
+public record RedisConfiguration
+{
+    public RedisConfiguration(string connectionString, string key, string streamField)
+    {
+        ConnectionString = connectionString;
+        Key = key;
+        StreamField = streamField;
+    }
+
+    public string ConnectionString { get; private set; }
+    public string Key { get; private set; }
+    public string StreamField { get; private set; }
+}
---
+++ SharedNET/RedisEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedNET;
+
+public record RedisEvent
+{
+    public RedisEvent(string origion, string message)
+    {
+        Origion = origion;
+        Message = message;
+    }
+
+    public string Origion { get; private set; }
+    public string Message { get; private set; }
+}
